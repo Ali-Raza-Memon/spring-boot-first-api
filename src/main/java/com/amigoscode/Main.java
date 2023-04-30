1aa064b122e77e72ccda227d22a246f2141a6365
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-04-30 22:08:10 +0500

    first API code in Spring Boot

---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,27 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+
+    public static void main(String args[]){
+
+        SpringApplication.run(Main.class,args);
+
+    }
+
+    @GetMapping("/")
+    private String Greet(){
+        return "Hello,this is first API code";
+    }
+
+
+
+
+}
